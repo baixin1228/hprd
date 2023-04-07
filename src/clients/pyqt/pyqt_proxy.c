259597e6f0d85bd033be71312f055a7acbe9fda9
Bai Xin <1228504957@qq.com>
2023-04-07 11:25:00 +0800

    refactor code.

--- src/clients/pyqt/pyqt_proxy.c
+++ src/clients/pyqt/pyqt_proxy.c
@@ -13,14 +13,6 @@
 #include "protocol.h"
 #include "net_help.h"
 
-int fd = -1;
-struct mem_pool client_pool = {0};
-struct display_object *dsp_obj = NULL;
-struct decodec_object *dec_obj = NULL;
-
-void *resize_python_self = NULL;
-void (* resize_callback)(void *oqu, uint32_t width, uint32_t height) = NULL;
-
 struct setting_request
 {
 	uint32_t set_id;
@@ -29,25 +21,34 @@ struct setting_request
 	struct setting_request *next;
 };
 
-pthread_mutex_t setting_lock = PTHREAD_MUTEX_INITIALIZER;
-struct setting_request *head = NULL;
+struct client {
+	int fd;
+	struct mem_pool client_pool;
+	struct display_object *dsp_obj;
+	struct decodec_object *dec_obj;
+	void *resize_python_self;
+	void (* resize_callback)(void *oqu, uint32_t width, uint32_t height);
+	struct setting_request *setting_head;
+	pthread_mutex_t setting_lock;
+} client = {0};
+
 void add_setting_request(struct setting_request *node)
 {
-	pthread_mutex_lock(&setting_lock);
-	if(head == NULL)
+	pthread_mutex_lock(&client.setting_lock);
+	if(client.setting_head == NULL)
 	{
-		head = node;
+		client.setting_head = node;
 	}else{
-		node->next = head;
-		head = node;
+		node->next = client.setting_head;
+		client.setting_head = node;
 	}
-	pthread_mutex_unlock(&setting_lock);
+	pthread_mutex_unlock(&client.setting_lock);
 }
 
 struct setting_request *get_del_setting_request(uint32_t set_id)
 {
-	pthread_mutex_lock(&setting_lock);
-	struct setting_request *ret = head;
+	pthread_mutex_lock(&client.setting_lock);
+	struct setting_request *ret = client.setting_head;
 	struct setting_request *last = ret;
 	while(ret)
 	{
@@ -59,7 +60,7 @@ struct setting_request *get_del_setting_request(uint32_t set_id)
 		last = ret;
 		ret = ret->next;
 	}
-	pthread_mutex_unlock(&setting_lock);
+	pthread_mutex_unlock(&client.setting_lock);
 	return ret;
 }
 
@@ -90,8 +91,8 @@ void _on_setting(int fd, struct setting_event *event)
 }
 
 static void _on_package(char *buf, size_t len) {
-	if(dec_obj) {
-		decodec_put_pkt(dec_obj, buf, len);
+	if(client.dec_obj) {
+		decodec_put_pkt(client.dec_obj, buf, len);
 	}
 }
 
@@ -108,7 +109,7 @@ static void _on_client_pkt(int fd, char *buf, size_t len) {
 			break;
 		}
 		case SETTING_CHANNEL: {
-			_on_setting(fd, (struct setting_event *)pkt->data);
+			_on_setting(client.fd, (struct setting_event *)pkt->data);
 			break;
 		}
 		default : {
@@ -131,26 +132,23 @@ int py_on_frame() {
 	int ret;
 	int buf_id;
 
-	if(tcp_recv_pkt(fd, _recv_buf, BUFLEN, _on_client_pkt) == -1)
-		exit(-1);
-
-	buf_id = decodec_get_fb(dec_obj);
+	buf_id = decodec_get_fb(client.dec_obj);
 	if(buf_id == -1) {
 		return 0;
 	}
 
-	ret = display_put_fb(dsp_obj, buf_id);
+	ret = display_put_fb(client.dsp_obj, buf_id);
 	if(ret != 0) {
 		log_error("display_put_fb fail.");
 		return -1;
 	}
-	buf_id = display_get_fb(dsp_obj);
+	buf_id = display_get_fb(client.dsp_obj);
 	if(buf_id == -1) {
 		log_error("display_get_fb fail.");
 		return -1;
 	}
 
-	ret = decodec_put_fb(dec_obj, buf_id);
+	ret = decodec_put_fb(client.dec_obj, buf_id);
 	if(ret != 0) {
 		log_error("decodec_put_fb fail.");
 		return -1;
@@ -172,19 +170,16 @@ int py_client_init_config(uint64_t winid) {
 	uint32_t frame_rate = 61;
 	GHashTable *fb_info = g_hash_table_new(g_str_hash, g_str_equal);
 
-	if(tcp_recv_pkt(fd, _recv_buf, BUFLEN, _on_client_pkt) == -1)
-		exit(-1);
-
-	if(decodec_get_info(dec_obj, fb_info) == -1) {
+	if(decodec_get_info(client.dec_obj, fb_info) == -1) {
 		log_info("client waiting dec info.");
 		goto END;
 	}
 
 	g_hash_table_insert(fb_info, "frame_rate", &frame_rate);
 	g_hash_table_insert(fb_info, "window", &winid);
-	display_set_info(dsp_obj, fb_info);
-	if(resize_callback != NULL)
-		resize_callback(resize_python_self,
+	display_set_info(client.dsp_obj, fb_info);
+	if(client.resize_callback != NULL)
+		client.resize_callback(client.resize_python_self,
 						   *(uint32_t *)g_hash_table_lookup(fb_info, "width"),
 						   *(uint32_t *)g_hash_table_lookup(fb_info, "height"));
 	ret = 0;
@@ -193,60 +188,75 @@ END:
 	return ret;
 }
 
+// static void _on_client_recv(int fd, char *buf, size_t len) {
+
+// }
+
+void *recv_thread(void *oq) {
+	while(tcp_recv_pkt(client.fd, _recv_buf, BUFLEN, _on_client_pkt) != -1);
+	log_warning("recv_thread exit.");
+	return NULL;
+}
+
 int py_client_connect(char *ip, uint16_t port) {
 	int ret;
 	int buf_id;
+	pthread_t p1;
 
 	debug_info_regist();
 
-	fd = client_connect(ip, port);
+	client.fd = client_connect(ip, port);
 
-	if (fd == -1) {
+	if (client.fd == -1) {
 		return -1;
 	}
 
-	dec_obj = decodec_init(&client_pool);
-	dsp_obj = display_dev_init(&client_pool, "x11_renderer");
+	pthread_mutex_init(&client.setting_lock, NULL);
+
+	client.dec_obj = decodec_init(&client.client_pool);
+	client.dsp_obj = display_dev_init(&client.client_pool, "x11_renderer");
 
 	for (int i = 0; i < 5; ++i) {
-		buf_id = alloc_buffer(&client_pool);
+		buf_id = alloc_buffer(&client.client_pool);
 		if(buf_id == -1) {
 			log_error("alloc_buffer fail.");
 			return -1;
 		}
-		ret = decodec_map_fb(dec_obj, buf_id);
+		ret = decodec_map_fb(client.dec_obj, buf_id);
 		if(ret != 0) {
 			log_error("decodec_map_fb fail.");
 			return -1;
 		}
-		ret = display_map_fb(dsp_obj, buf_id);
+		ret = display_map_fb(client.dsp_obj, buf_id);
 		if(ret != 0) {
 			log_error("display_map_fb fail.");
 			return -1;
 		}
 	}
 
+	pthread_create(&p1, NULL, recv_thread, NULL);
+	pthread_detach(p1);
 	return 0;
 }
 
 int py_client_resize(uint32_t width, uint32_t height) {
-	if(!dsp_obj)
+	if(!client.dsp_obj)
 		return -1;
 
-	return display_resize(dsp_obj, width, height);
+	return display_resize(client.dsp_obj, width, height);
 }
 
 int py_client_regist_stream_size_cb(void *oqu, void (*callback)(void *oqu,
 									uint32_t width, uint32_t height)) {
-	resize_python_self = oqu;
-	resize_callback = callback;
+	client.resize_python_self = oqu;
+	client.resize_callback = callback;
 	return 0;
 }
 
 int py_client_close() {
-	if(fd != -1) {
-		close(fd);
-		fd = -1;
+	if(client.fd != -1) {
+		close(client.fd);
+		client.fd = -1;
 	}
 
 	return 0;
@@ -259,7 +269,7 @@ int py_mouse_move(int x, int y) {
 	event.x = x;
 	event.y = y;
 
-	if(send_event(fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
+	if(send_event(client.fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
 			== -1) {
 		log_error("send_event fail.");
 		return -1;
@@ -281,7 +291,7 @@ int py_mouse_click(int x, int y, int key, int down_or_up) {
 	event.x = x;
 	event.y = y;
 
-	if(send_event(fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
+	if(send_event(client.fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
 			== -1) {
 		log_error("send_event fail.");
 		return -1;
@@ -296,7 +306,7 @@ int py_wheel_event(int key) {
 	event.type = MOUSE_WHEEL;
 	event.key_code = key;
 
-	if(send_event(fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
+	if(send_event(client.fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
 			== -1) {
 		log_error("send_event fail.");
 		return -1;
@@ -316,7 +326,7 @@ int py_key_event(int key, int down_or_up) {
 
 	event.key_code = key;
 
-	if(send_event(fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
+	if(send_event(client.fd, INPUT_CHANNEL, (char *)&event, sizeof(event))
 			== -1) {
 		log_error("send_event fail.");
 		return -1;
@@ -355,7 +365,7 @@ int _send_setting_data(void *oqu, uint32_t type, uint32_t frame_rate,
 	event.cmd = type;
 	event.set_id = htonl(set_id);
 	event.value = htonl(frame_rate);
-	if(send_event(fd, SETTING_CHANNEL, (char *)&event, sizeof(event))
+	if(send_event(client.fd, SETTING_CHANNEL, (char *)&event, sizeof(event))
 			== -1) {
 		log_error("send_event fail.");
 		set_req = get_del_setting_request(set_id);

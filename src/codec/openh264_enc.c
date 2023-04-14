54c718f075b426fc0da73fca810aec6f01c163fa
Bai Xin <1228504957@qq.com>
2023-04-14 13:14:38 +0800

    codec: add openh264 encodec.

--- src/codec/openh264_enc.c
+++ src/codec/openh264_enc.c
@@ -15,6 +15,7 @@
 struct openh264_enc_data {
 	ISVCEncoder *encoder;
 	SEncParamBase param;
+	SEncParamExt paramExt;
 	SSourcePicture pic;
 	SFrameBSInfo info;
 	uint32_t fb_format;
@@ -22,6 +23,9 @@ struct openh264_enc_data {
 	uint32_t hor_stride;
 	uint32_t height;
 	uint32_t ver_stride;
+	uint64_t frame_count;
+	uint16_t gop_size;
+	uint16_t thread_count;
 
 	struct SwsContext *sws_ctx;
 	uint8_t * frame_data[4];
@@ -75,7 +79,8 @@ uint32_t com_to_svc_fmt(uint32_t format) {
 	return videoFormatI420;
 }
 
-static int openh264_enc_set_info(
+__attribute__((unused))
+static int openh264_enc_set_info_simple(
 	struct encodec_object *obj, GHashTable *enc_info) {
 	uint32_t width;
 	uint32_t height;
@@ -144,7 +149,115 @@ static int openh264_enc_set_info(
 	return 0;
 }
 
+static int openh264_enc_set_info(
+	struct encodec_object *obj, GHashTable *enc_info) {
+	uint32_t width;
+	uint32_t height;
+	uint32_t bit_rate;
+	uint32_t frame_rate;
+	uint32_t stream_format;
+	WelsTraceCallback callback_function;
+	struct openh264_enc_data *priv = obj->priv;
+
+	if(g_hash_table_contains(enc_info, "frame_rate"))
+		frame_rate = *(uint32_t *)g_hash_table_lookup(enc_info, "frame_rate");
+	else
+		frame_rate = 30;
+
+	if(g_hash_table_contains(enc_info, "gop_size"))
+		priv->gop_size = *(uint32_t *)g_hash_table_lookup(enc_info, "gop_size");
+	else
+		priv->gop_size = 60;
+
+	if(g_hash_table_contains(enc_info, "thread_count"))
+		priv->thread_count = *(uint32_t *)g_hash_table_lookup(enc_info, "thread_count");
+	else
+		priv->thread_count = 1;
+
+	stream_format = *(uint32_t *)g_hash_table_lookup(enc_info, "stream_fmt");
+	if(stream_format != STREAM_H264) {
+		log_error("stream_format is invalid.%d %d", stream_format, STREAM_H264);
+		return -1;
+	}
+
+	bit_rate = *(uint32_t *)g_hash_table_lookup(enc_info, "bit_rate");
+	width = *(uint32_t *)g_hash_table_lookup(enc_info, "width");
+	height = *(uint32_t *)g_hash_table_lookup(enc_info, "height");
+	priv->fb_format = com_to_svc_fmt(*(uint32_t *)g_hash_table_lookup(enc_info, "format"));
+
+	priv->sws_ctx = sws_getContext(
+						width,
+						height,
+						AV_PIX_FMT_RGB32,
+						width,
+						height,
+						AV_PIX_FMT_YUV420P,
+						SWS_POINT, NULL, NULL, NULL);
+	priv->linesize[0] = width;
+	priv->linesize[1] = width / 2;
+	priv->linesize[2] = width / 2;
+	priv->linesize[3] = 0;
+	priv->frame_data[0] = malloc(width * height * 4);
+	priv->frame_data[1] = malloc(width * height);
+	priv->frame_data[2] = malloc(width * height);
+	priv->frame_data[3] = NULL;
+
+	(*priv->encoder)->GetDefaultParams(priv->encoder, &priv->paramExt);
+
+	priv->paramExt.iUsageType				= SCREEN_CONTENT_REAL_TIME;
+	priv->paramExt.fMaxFrameRate			= frame_rate;
+	priv->paramExt.iPicWidth				= width;
+	priv->paramExt.iPicHeight				= height;
+	priv->paramExt.iTargetBitrate			= bit_rate;
+	priv->paramExt.iRCMode					= RC_BITRATE_MODE;
+	priv->paramExt.iComplexityMode			= LOW_COMPLEXITY;
+	priv->paramExt.uiIntraPeriod			= priv->gop_size;
+    priv->paramExt.iMultipleThreadIdc		= priv->thread_count;
+    priv->paramExt.iEntropyCodingModeFlag	= 0; /* CAVLC */
+    priv->paramExt.bEnableFrameSkip			= 0;
+    priv->paramExt.iNumRefFrame				= -1;
+    priv->paramExt.bEnableSSEI				= 1;
+    priv->paramExt.iMaxQp					= 51;
+    priv->paramExt.iMinQp					= 18;
+    priv->paramExt.bEnableAdaptiveQuant		= 1;
+    priv->paramExt.iTemporalLayerNum		= 1;
+
+	// 空间层
+	SSpatialLayerConfig *spaticalLayerCfg = &(priv->paramExt.sSpatialLayers[SPATIAL_LAYER_0]);
+	spaticalLayerCfg->iVideoWidth = width;
+	spaticalLayerCfg->iVideoHeight = height;
+	spaticalLayerCfg->fFrameRate = frame_rate;
+	spaticalLayerCfg->iSpatialBitrate = priv->paramExt.iTargetBitrate;
+	spaticalLayerCfg->iMaxSpatialBitrate = priv->paramExt.iMaxBitrate;
+	spaticalLayerCfg->uiProfileIdc= PRO_BASELINE;
+	
+	// 单slice，有的解码器无法解码多slice
+	spaticalLayerCfg->sSliceArgument.uiSliceMode = SM_SINGLE_SLICE;
+
+
+	// 日志级别
+	uint32_t log_level = WELS_LOG_ERROR;
+	(*priv->encoder)->SetOption(priv->encoder,
+								DECODER_OPTION_TRACE_LEVEL, &log_level);
+	callback_function = _libopenh264_trace_callback;
+	(*priv->encoder)->SetOption(priv->encoder,
+								DECODER_OPTION_TRACE_CALLBACK, (void *)&callback_function);
+	(*priv->encoder)->SetOption(priv->encoder,
+								DECODER_OPTION_TRACE_CALLBACK_CONTEXT, (void *)obj);
+
+	// 初始化
+	if (cmResultSuccess != (*priv->encoder)->InitializeExt(priv->encoder, &priv->paramExt)) {
+		log_error("InitializeExt fail.");
+		return -1;
+	}
+	return 0;
+}
+
 static int openh264_enc_map_buf(struct encodec_object *obj, int buf_id) {
+	return 0;
+}
+
+static int openh264_enc_unmap_buf(struct encodec_object *obj, int buf_id) {
 	return 0;
 }
 
@@ -173,7 +286,7 @@ static int openh264_frame_enc(struct encodec_object *obj, int buf_id) {
 	priv->pic.pData[2] = priv->frame_data[2];
 	priv->pic.pData[3] = priv->frame_data[3];
 
-	int rv = (*priv->encoder)->EncodeFrame (priv->encoder, &priv->pic, &priv->info);
+	int rv = (*priv->encoder)->EncodeFrame(priv->encoder, &priv->pic, &priv->info);
 	if (rv != cmResultSuccess) {
 		log_error("encodec frame fail.");
 		return -1;
@@ -192,6 +305,16 @@ static int openh264_frame_enc(struct encodec_object *obj, int buf_id) {
 	while(queue_put_int(&priv->id_queue, buf_id) != 0)
 		log_warning("queue_put_int fail.");
 
+	// if(priv->frame_count % priv->gop_size == 0)
+	// {
+	// 	rv = (*priv->encoder)->ForceIntraFrame(priv->encoder, true);
+	// 	if (rv != cmResultSuccess) {
+	// 		log_error("encodec frame fail.");
+	// 		return -1;
+	// 	}
+	// }
+
+	priv->frame_count++;
 	return 0;
 }
 
@@ -217,6 +340,7 @@ struct encodec_ops openh264_enc_ops = {
 	.init               = openh264_enc_init,
 	.set_info           = openh264_enc_set_info,
 	.map_buffer         = openh264_enc_map_buf,
+	.unmap_buffer       = openh264_enc_unmap_buf,
 	.put_buffer         = openh264_frame_enc,
 	.get_buffer         = openh264_enc_getbuf,
 	.release            = openh264_enc_release
